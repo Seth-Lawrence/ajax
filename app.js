68e72ff935bda24b0bfcf3dfbf7cf05b634cedb1
Seth Hobson <sethlawrencehobson@gmail.com>
2023-11-14 20:07:09 -0500

    started async function

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+"use strict";
+
+async function getGiphy() {
+
+  const giphy = $('#search-term').value();
+  const params = new URLSearchParams({giphy});
+  const response = await fetch(`http://api.giphy.com/v1/gifs/search?q=${params}`);
+  const searchResult = await response.json();
+
+  console.log(params);
+
+}
+
 console.log("Let's get this party started!");
