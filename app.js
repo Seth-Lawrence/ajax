a99e6bb5e33a0ecce94944556c6f53cdb52a93f0
jswwWorks <jsww.works@gmail.com>
2023-11-14 20:34:41 -0500

    Completed Part 1 of AJAX exercise.

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
 "use strict";
 
-async function getGiphy() {
 
-  const giphy = $('#search-term').value();
+/** Takes nothing. Grabs search term, uses it to get results from GIPHY,
+ *  returns X.
+ */
+async function getGiphy(evt) {
+
+  evt.preventDefault();
+
+  // Find search value input
+  const giphy = $('#search-term').val();
+
+  // Generate URL for fetch
   const params = new URLSearchParams({giphy});
-  const response = await fetch(`http://api.giphy.com/v1/gifs/search?q=${params}`);
-  const searchResult = await response.json();
 
+  // Key for Giphy
+  const apiKey = "&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym";
+
+  const response = await fetch(
+    `http://api.giphy.com/v1/gifs/search?q=${params}${apiKey}`
+  );
+
+  // turn found JSON to
+  const searchResult = await response.json();
+  console.log(`http://api.giphy.com/v1/gifs/search?q=rest&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`);
+  console.log(searchResult);
   console.log(params);
 
 }
 
-console.log("Let's get this party started!");
+// Add event listener
+$("#submit-form").on("click", getGiphy);
